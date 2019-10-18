db48c30405766607f5c42a79f67493dc336654bf
renato <renatoalvesbelem@gmail.com>
2019-10-17 22:22:26 -0300

    Adiciona menu de prova

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -96,7 +96,11 @@
                                 </a>
                             </div>
                         </li>
-
+                        <li class="nav-item dropdown">
+                            <a class="nav-link" href="{{ route('questao/relatorio') }}">
+                                {{ __('Prova') }}
+                            </a>
+                        </li>
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
