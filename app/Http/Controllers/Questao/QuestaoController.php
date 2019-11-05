56688d9fd931be55ce1d76ecdc836cf0c7bd4498
renato <renatoalvesbelem@gmail.com>
2019-11-04 21:56:16 -0300

    Adicionado ação para abrir prova em uma nova página

--- app/Http/Controllers/Questao/QuestaoController.php
+++ app/Http/Controllers/Questao/QuestaoController.php
@@ -78,9 +78,10 @@ class QuestaoController extends Controller
             DB::commit();
         } catch (\Exception $ex) {
             DB::rollback();
-            return redirect()->route('questao.index')->withSuccess("Houve um erro no cadastro da questão.");
+            return redirect()->route('questao.index')->withErrors("Erro ao cadastrar questão.");
         }
-        return redirect()->route('questao.index')->withErrors("Erro ao cadastrar questão.");
+        return redirect()->route('questao.index')->withSuccess("Questão '$questao->idQuestao' cadastrada com sucesso.");
+
     }
 
     /**
